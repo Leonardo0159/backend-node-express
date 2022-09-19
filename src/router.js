8b2fef8fd563a5c57b309a9f3a78c92acfd18b22
Leonardo <leo.sn159@gmail.com>
2022-09-19 12:41:26 -0300

    Primeiro commit

---
+++ src/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/taskMiddleware');
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id',tasksMiddleware.validateTitle, tasksMiddleware.validateStatus, tasksController.updateTask);
+
+
+module.exports = router;
